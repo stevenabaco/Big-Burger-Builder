78c85c41a6535f4842a2ed1d20a0038b7249606a
= <stevenabaco@gmail.com>
2021-01-06 10:21:49 -0600

    Remove Console.logs not needed anymore

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,8 +6,6 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = props => {
     const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
         console.log([...Array(props.ingredients[ingKey])].map(( _, i) => {
-            console.log(ingKey)
-            console.log(i)
             return <BurgerIngredient key={ingKey + i} type={ingKey} />
             
     })) 
