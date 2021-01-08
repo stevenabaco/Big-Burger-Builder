f38bd7ab1bff057fcba0f4d57db990f96e770a14
= <stevenabaco@gmail.com>
2021-01-08 12:10:49 -0600

    Add Price to the Order Summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,11 +93,11 @@ class BurgerBuilder extends Component {
 				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
 					<OrderSummary
 						ingredients={this.state.ingredients}
+						price={this.state.totalPrice}
 						purchaseCancelled={this.purchaseCancelHandler}
 						purchaseContinued={this.purchaseContinueHandler}
 					/>
 				</Modal>{" "}
-				/>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
 					ingredientAdded={this.addIngredientHandler}
