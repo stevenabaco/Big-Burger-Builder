f2cf4231540075a14ed2a243ebc1cd03ede8c824
= <stevenabaco@gmail.com>
2021-01-07 23:34:59 -0600

    Implement backdrop comoponent

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,6 +28,10 @@ class BurgerBuilder extends Component {
 		this.setState({ purchasing: true });
 	}
 
+	purchaseCancelHandler = () => {
+		this.setState({ purchasing: false });
+	}
+
 	updatePurchaseState(ingredients) {
 		const sum = Object.keys(ingredients)
 			.map(ingKey => {
@@ -82,7 +86,7 @@ class BurgerBuilder extends Component {
 		} // {lettuce: true, meat: false}
 		return (
 			<>
-				<Modal show={this.state.purchasing}>
+				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
 					<OrderSummary ingredients={this.state.ingredients} />
 				</Modal>{" "}
 				/>
