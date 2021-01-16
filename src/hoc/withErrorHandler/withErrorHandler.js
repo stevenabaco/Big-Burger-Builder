3b1e6060c61d3d544eaaf3a8988d1d3497bda1a0
= <stevenabaco@gmail.com>
2021-01-15 20:39:09 -0600

    Add Fetching Orders logic

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,24 +8,27 @@ const withErrorHandler = (WrappedComponent, axios) => {
 			error: null,
 		};
 
-		componentDidMount() {
+		componentWillMount() {
 			this.reqInterceptor = axios.interceptors.request.use(req => {
-                this.setState({ error: null });
-                return req;
+				this.setState({ error: null });
+				return req;
 			});
-			this.resInterceptor = axios.interceptors.response.use(res => res, error => {
-				this.setState({ error: error });
-			});
-        }
-        
-        componentWillUnmount() {
-            axios.interceptors.request.eject(this.reqInterceptor);
-            axios.interceptors.request.eject(this.resInterceptor);
-        }
-
-		errorConfirmedHandler() {
-			this.setState({ error: null });
+			this.resInterceptor = axios.interceptors.response.use(
+				res => res,
+				error => {
+					this.setState({ error: error });
+				}
+			);
 		}
+
+		componentWillUnmount() {
+			axios.interceptors.request.eject(this.reqInterceptor);
+			axios.interceptors.request.eject(this.resInterceptor);
+		}
+
+		errorConfirmedHandler = () => {
+			this.setState({ error: null });
+		};
 		render() {
 			return (
 				<React.Fragment>
