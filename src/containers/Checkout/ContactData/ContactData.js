c3a892ac2f13ab35dc72c1bd3a3db34b7710766b
= <stevenabaco@gmail.com>
2021-01-16 20:08:32 -0600

    Create inputs based on JS Config

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,33 +84,25 @@ class ContactData extends Component {
 	};
 
 	render() {
+		const formElementsArray = [];
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key],
+
+			})
+		}
 		let form = (
 			<form>
-				<Input
-					elementType="..."
-					elementConfig="..."
-					value="..."
-				/>
-				<Input
-					inputtype='input'
-					type='email'
-					name='email'
-					placeholder='Your Email'
-				/>
-				<Input
-					inputtype='input'
-					type='text'
-					name='street'
-					placeholder='Address'
-				/>
-				<Input inputtype='input' type='text' name='city' placeholder='City' />
-				<Input inputtype='input' type='text' name='state' placeholder='State' />
-				<Input
-					inputtype='input'
-					type='text'
-					name='zip'
-					placeholder='Zip Code'
-				/>
+				{formElementsArray.map(formElement => (
+					<Input
+						key={formElement.id}
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.value}
+					/>
+				))}
+				
 				<Button btnType='Success' clicked={this.orderHandler}>
 					ORDER
 				</Button>
