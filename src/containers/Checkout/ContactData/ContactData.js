45728b7036a7030b64b59f4b1f2a03278e1369d6
= <stevenabaco@gmail.com>
2021-01-16 23:23:31 -0600

    Add Validation Rules

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -16,6 +16,10 @@ class ContactData extends Component {
 					placeholder: "Your Name",
 				},
 				value: "",
+				validation: {
+					required: true,
+				},
+				valid: false,
 			},
 			street: {
 				elementType: "input",
@@ -24,6 +28,10 @@ class ContactData extends Component {
 					placeholder: "Street",
 				},
 				value: "",
+				validation: {
+					required: true,
+				},
+				valid: false,
 			},
 			zipCode: {
 				elementType: "input",
@@ -32,6 +40,12 @@ class ContactData extends Component {
 					placeholder: "Zip Code",
 				},
 				value: "",
+				validation: {
+					required: true,
+					minLength: 5,
+					maxLength: 5,
+				},
+				valid: false,
 			},
 			country: {
 				elementType: "input",
@@ -40,6 +54,10 @@ class ContactData extends Component {
 					placeholder: "Country",
 				},
 				value: "",
+				validation: {
+					required: true,
+				},
+				valid: false,
 			},
 			email: {
 				elementType: "input",
@@ -48,6 +66,10 @@ class ContactData extends Component {
 					placeholder: "Your E-mail",
 				},
 				value: "",
+				validation: {
+					required: true,
+				},
+				valid: false,
 			},
 			deliveryMethod: {
 				elementType: "select",
@@ -70,7 +92,9 @@ class ContactData extends Component {
 		this.setState({ loading: true });
 		const formData = {};
 		for (let formElementIdentifier in this.state.orderForm) {
-			formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+			formData[formElementIdentifier] = this.state.orderForm[
+				formElementIdentifier
+			].value;
 		}
 		const order = {
 			ingredients: this.props.ingredients,
@@ -88,6 +112,22 @@ class ContactData extends Component {
 			});
 	};
 
+	checkValidity(value, rules) {
+		let isValid = true;
+
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid;
+		}
+
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength && isValid;
+		}
+		if (rules.maxLength) {
+			isValid = value.length <= rules.maxLength && isValid;
+		}
+		return isValid
+	}
+
 	inputChangedHandler = (event, input) => {
 		const updatedOrderForm = {
 			...this.state.orderForm,
@@ -96,7 +136,9 @@ class ContactData extends Component {
 			...updatedOrderForm[input],
 		};
 		updatedFormElement.value = event.target.value;
+		updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation );
 		updatedOrderForm[input] = updatedFormElement;
+		console.log(updatedFormElement);
 		this.setState({ orderForm: updatedOrderForm });
 	};
 
@@ -120,9 +162,7 @@ class ContactData extends Component {
 					/>
 				))}
 
-				<Button btnType='Success'>
-					ORDER
-				</Button>
+				<Button btnType='Success'>ORDER</Button>
 			</form>
 		);
 		if (this.state.loading) {
