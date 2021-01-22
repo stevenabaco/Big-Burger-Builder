76864462d6bc602d1400cbb655a37b0768f502ef
= <stevenabaco@gmail.com>
2021-01-21 22:06:36 -0600

    setup axios to send Post to Server to create a new user

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,27 +1,46 @@
-import * as actionTypes from './actionTypes';
+import axios from "axios";
+import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    };
+	return {
+		type: actionTypes.AUTH_START,
+	};
 };
 
 export const authSuccess = (authData) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        authData: authData
-    }
+	return {
+		type: actionTypes.AUTH_SUCCESS,
+		authData: authData,
+	};
 };
 
 export const authFail = (error) => {
-    return {
-        type: actionTypes.AUTH_FAIL,
-        error: error
-    };
+	return {
+		type: actionTypes.AUTH_FAIL,
+		error: error,
+	};
 };
 
 export const auth = (email, password) => {
-    return dispatch => {
-        dispatch(authStart());
-    };
+	return (dispatch) => {
+		dispatch(authStart());
+		const authData = {
+			email: email,
+			password: password,
+			returnSecureToken: true,
+		};
+		axios
+			.post(
+				"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBfkt0CMYosSmnJghts24IHi0ZNMc77VuI",
+				authData
+			)
+			.then((response) => {
+				console.log(response);
+				dispatch(authSuccess(response.data));
+			})
+			.catch((err) => {
+				console.log(err);
+				dispatch(authFail(err));
+			});
+	};
 };
