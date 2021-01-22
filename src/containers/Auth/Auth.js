4df145510388056ce0f46b0770bcf7aaa80ceebc
= <stevenabaco@gmail.com>
2021-01-22 10:44:48 -0600

    Add Spinner and Login error message handling

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from "./Auth.module.css";
 import * as actions from "../../store/actions/index";
 
@@ -100,7 +101,7 @@ class Auth extends Component {
 			});
 		}
 
-		const form = formElementsArray.map((formElement) => (
+		let form = formElementsArray.map((formElement) => (
 			<Input
 				key={formElement.id}
 				elementType={formElement.config.elementType}
@@ -110,11 +111,26 @@ class Auth extends Component {
 				shouldValidate={formElement.config.validation}
 				touched={formElement.config.touched}
 				changed={(event) => this.inputChangedHandler(event, formElement.id)}
-			/>
-		));
+            />
+    
+        ));
+        
+                
+        if (this.props.loading) {
+            form = <Spinner />;
+        }
+
+        let errorMessage = null;
+
+        if (this.props.error) {
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            )
+        }
 
 		return (
-			<div className={classes.Auth}>
+            <div className={classes.Auth}>
+                {errorMessage}
 				<form onSubmit={this.submitHandler}>
 					{form}
 					<Button btnType="Success">SUBMIT</Button>
@@ -127,6 +143,13 @@ class Auth extends Component {
 	}
 }
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
+
 const mapDispatchToProps = (dispatch) => {
 	return {
 		onAuth: (email, password, isSignup) => {
@@ -135,4 +158,4 @@ const mapDispatchToProps = (dispatch) => {
 	};
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
