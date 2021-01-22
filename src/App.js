4c4ad5271e7dd78d46e788e72c9c580e22b50342
= <stevenabaco@gmail.com>
2021-01-21 20:19:45 -0600

    Add Authentication Actions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from './containers/Checkout/Checkout';
-import { Route, Switch } from 'react-router-dom';
-import Orders from './containers/Orders/Orders';
+import Checkout from "./containers/Checkout/Checkout";
+import {Route, Switch} from "react-router-dom";
+import Orders from "./containers/Orders/Orders";
+import Auth from "./containers/Auth/Auth";
 
 class App extends Component {
 	render() {
@@ -11,9 +12,10 @@ class App extends Component {
 			<div>
 				<Layout>
 					<Switch>
-						<Route path='/checkout' component={Checkout} />
-						<Route path='/orders' component={Orders} />
-						<Route exact path='/' component={BurgerBuilder} />
+						<Route path="/checkout" component={Checkout} />
+						<Route path="/orders" component={Orders} />
+						<Route path="/auth" component={Auth} />
+						<Route exact path="/" component={BurgerBuilder} />
 					</Switch>
 				</Layout>
 			</div>
