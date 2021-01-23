93691b1f2bd51cb9cb838fa70003a0e3302959e3
= <stevenabaco@gmail.com>
2021-01-23 11:49:13 -0600

    Add Logic to show Orders only if authenticated

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import NavigationItem from './NavigationItem/NavigationItem';
-import classes from './NavigationItems.module.css'
+import React from "react";
+import NavigationItem from "./NavigationItem/NavigationItem";
+import classes from "./NavigationItems.module.css";
 
 const navigationItems = (props) => (
-    <ul className={classes.NavigationItems}>
-      <NavigationItem link='/' exact>Burger Builder</NavigationItem>
-      <NavigationItem link='/orders' >Orders</NavigationItem>
-    { !props.isAuthenticated
-      ? <NavigationItem link='/auth' >Authenticate</NavigationItem>
-      : <NavigationItem link='/logout'>Logout</NavigationItem>
-    }
-    </ul>
+	<ul className={classes.NavigationItems}>
+		<NavigationItem link="/" exact>
+			Burger Builder
+		</NavigationItem>
+		{props.isAuthenticated ? (
+			<NavigationItem link="/orders">Orders</NavigationItem>
+		) : null}
+		{!props.isAuthenticated ? (
+			<NavigationItem link="/auth">Authenticate</NavigationItem>
+		) : (
+			<NavigationItem link="/logout">Logout</NavigationItem>
+		)}
+	</ul>
 );
 
-export default navigationItems
+export default navigationItems;
