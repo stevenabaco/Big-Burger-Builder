0bb98bf54811eb655934f9b27c7499dff3bbea4a
= <stevenabaco@gmail.com>
2021-01-26 22:33:33 -0600

    Fix remove button to subtract

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,15 +5,15 @@ import classes from "./NavigationItems.module.css";
 const navigationItems = (props) => (
 	<ul className={classes.NavigationItems}>
 		<NavigationItem link="/" exact>
-			Burger Builder
+			BURGER BUILDER
 		</NavigationItem>
 		{props.isAuthenticated ? (
-			<NavigationItem link="/orders">Orders</NavigationItem>
+			<NavigationItem link="/orders">ORDERS</NavigationItem>
 		) : null}
 		{!props.isAuthenticated ? (
-			<NavigationItem link="/auth">Authenticate</NavigationItem>
+			<NavigationItem link="/auth">LOGIN</NavigationItem>
 		) : (
-			<NavigationItem link="/logout">Logout</NavigationItem>
+			<NavigationItem link="/logout">LOGOUT</NavigationItem>
 		)}
 	</ul>
 );
