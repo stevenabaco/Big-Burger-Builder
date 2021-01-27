b2e1fefb633574dfe613db55a01b3eecc031e0a3
= <stevenabaco@gmail.com>
2021-01-27 00:36:09 -0600

    Add CSS styles for UI/UX

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,15 +14,17 @@ const BuildControls = (props) => (
 		<p>
 			<strong>Current Price: $ {props.price.toFixed(2)}</strong>
 		</p>
-		{controls.map((ctrl) => (
+		<div className={classes.BuildControlContainer}>{controls.map((ctrl) => (
 			<BuildControl
 				key={ctrl.label}
 				label={ctrl.label}
 				added={() => props.ingredientAdded(ctrl.type)}
 				removed={() => props.ingredientRemoved(ctrl.type)}
 				disabled={props.disabled[ctrl.type]}
+				
 			/>
 		))}
+		</div>
 		<button
 			className={classes.OrderButton}
 			disabled={!props.purchasable}
